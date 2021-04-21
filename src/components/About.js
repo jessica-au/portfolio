845a1f88c8b7f7be4ae5dd058cea0e38265c9b05
jessica-au <AU.JESSICA.M@GMAIL.COM>
2021-04-20 23:33:06 -0700

    brand statement on about page

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div className="about">
-            This is about me
+            I am an educator turned full-stack developer who maintains a strong passion for teaching and creating equitable opportunities for learning. I value developing ed tech tools that augment and transform the learning experience, and I hope to lend my prior teaching experience to the development of tools that create greater educational equity.
         </div>
     );
 }
