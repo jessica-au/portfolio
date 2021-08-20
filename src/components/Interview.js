174517764e8949bdabb8176348d7cc79f7df0f37
jessica-au <AU.JESSICA.M@GMAIL.COM>
2021-08-19 17:33:22 -0700

    Add interview component

---
+++ src/components/Interview.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Resume from './Resume';
+
+
+function Interview() {
+    return (
+        <>
+        <h1 class="titles">Q&A Time with Jessica</h1>
+        <div>
+            So Jessica, you're interviewing yourself for prospective employers?
+
+            Yep.  Looks like it.
+
+            Pretty cool.  So tell me a bit about yourself.  
+
+            Well Jessica, it's been a non-linear path to software engineering.  I graduated from UC Berkeley with a degree in, get this, English.  And a few years later, I attended CSUS for a single subject teaching credential in English.  I taught within the public education system for some years before switching to the private sector.  
+            
+            Sounds like you've seen a lot within the educational sphere! What made you decide to change paths? 
+            
+            COVID hit. I had time to consider my path, pursue passion interests, and it kind of snowballed from there.  I have a great passion for learning and for languages!  Quarantine and shelter-in-place allowed me to do a deep dive into linguistics, which kind of naturally led to programmatic languages, and before I knew it, I was finishing up a Software Engineering Immersive Program through General Assembly!
+
+            
+        
+        </div>
+        </>
+    )
+}
+export default Resume;
